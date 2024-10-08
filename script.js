25f8850a36cf7342030ab59cd4919c7b3da71258
Bekislam09 <email@example.com>
2024-10-08 16:18:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+]
+// Основное дз
+
+// users.forEach(user => {
+//     user.admin = false
+// })
+
+// console.log(users);
+
+
+// доп дз
+
+// users.forEach(user => {
+//     user.admin = user.name == 'Маша'
+// })
+
+
+// console.log(users);
